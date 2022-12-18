8778369808381a1c8e14ddefec1357503b5ad3a4
Aditya <adi.kg2@gmail.com>
2022-12-18 11:29:49 +0530

    added responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,9 @@ import MovieInput from "./MovieInput";
 const NavBar = (props) => {
   return (
     <nav className="navigation-bar">
-      <img className="navbar-company-logo" src="./logo.png" alt="InSynk Logo" />
+      <span className="navbar-company-logo-container">
+        <img className="navbar-company-logo" src="./logo.png" alt="InSynk Logo" />
+      </span>
       <MovieInput setSearchMovieName={props.setSearchMovieName} />
     </nav>
   );
