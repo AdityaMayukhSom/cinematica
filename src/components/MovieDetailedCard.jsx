a7cbd0807321025b001b8d25a2a61d72e187cb33
Aditya <adi.kg2@gmail.com>
2022-12-18 13:40:03 +0530

    changed to async await

--- src/components/MovieDetailedCard.jsx
+++ src/components/MovieDetailedCard.jsx
@@ -1,97 +1,86 @@
 import { useState, useEffect } from "react";
 
 const MovieDetailedCard = (props) => {
-  let modifiedDateString, releaseDateString;
+	let modifiedDateString, releaseDateString;
 
-  const POSTER_BASE_URL = "https://image.tmdb.org/t/p/w780";
+	const POSTER_BASE_URL = "https://image.tmdb.org/t/p/w780";
 
-  const [movieName, setMovieName] = useState("");
-  const [movieRating, setMovieRating] = useState(0);
-  const [movieReleaseDate, setMovieReleaseDate] = useState("");
-  const [movieOverView, setMovieOverView] = useState("");
-  const [movieVoteCount, setMovieVoteCount] = useState(0);
-  const [moviePosterURL, setMoviePosterURL] = useState("");
+	const [movieName, setMovieName] = useState("");
+	const [movieRating, setMovieRating] = useState(0);
+	const [movieReleaseDate, setMovieReleaseDate] = useState("");
+	const [movieOverView, setMovieOverView] = useState("");
+	const [movieVoteCount, setMovieVoteCount] = useState(0);
+	const [moviePosterURL, setMoviePosterURL] = useState("");
 
-  const getMovieDetailsForDetailedCard = (movieID) => {
-    fetch(`/.netlify/functions/get-movie-details?movieID=${movieID}`)
-      .then((result) => {
-        return result.json();
-      })
-      .then((result) => {
-        return result.movieDetails;
-      })
-      .then((data) => {
-        setMovieName(data.original_title);
-        releaseDateString = new Date(data.release_date)
-          .toDateString()
-          .toString();
-        modifiedDateString =
-          releaseDateString.substring(4, 10) +
-          ", " +
-          releaseDateString.substring(10);
-        setMovieReleaseDate(modifiedDateString);
-        setMovieRating(data.vote_average.toFixed(1));
+	const getMovieDetailsForDetailedCard = async (movieID) => {
+		try {
+			const result = await fetch(`/.netlify/functions/get-movie-details?movieID=${movieID}`)
+			const objectFromJson = await result.json();
+			const data = objectFromJson.movieDetails;
+			setMovieName(data.original_title);
+			releaseDateString = new Date(data.release_date).toDateString().toString();
+			modifiedDateString = releaseDateString.substring(4, 10) + ", " + releaseDateString.substring(10);
+			setMovieReleaseDate(modifiedDateString);
+			setMovieRating(data.vote_average.toFixed(1));
+			setMovieOverView(data.overview);
+			setMovieVoteCount(data.vote_count);
+			setMoviePosterURL("".concat(POSTER_BASE_URL, data.poster_path));
+		} catch (err) {
+			console.log(err);
+			console.log("Error Occured");
+		};
+	}
 
-        setMovieOverView(data.overview);
-        setMovieVoteCount(data.vote_count);
-        setMoviePosterURL("".concat(POSTER_BASE_URL, data.poster_path));
-      })
-      .catch((err) => {
-        console.log(err);
-        console.log("Error Occured");
-      });
-  }
+	useEffect(() => {
+		getMovieDetailsForDetailedCard(props.movieID);
+	}, [props.shouldDetailedCardShow, props.movieID]);
 
-  useEffect(() => {
-    getMovieDetailsForDetailedCard(props.movieID);
-  }, [props.shouldDetailedCardShow, props.movieID]);
-
-  return (
-    <div
-      className="modal-background"
-      onClick={() => {
-        props.setShouldDetailedCardShow(false);
-      }}
-    >
-      <article
-        className="modal-container"
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
-      >
-        <div className="modal-name-and-close-container">
-          <span className="modal-movie-name">{movieName}</span>
-          <img
-            className="modal-close-button"
-            src="./multiply.svg"
-            alt=""
-            onClick={() => {
-              props.setShouldDetailedCardShow(false);
-            }}
-          />
-        </div>
-        <div className="modal-movie-description">
-          <span className="modal-movie-poster-container">
-            <img
-              className="modal-movie-poster"
-              src={moviePosterURL}
-              alt="Movie Poster"
-            />
-          </span>
-          <div className="modal-movie-description-text">
-            <p style={{ marginTop: "10px", marginBottom: "10px" }}>
-              <span style={{ fontWeight: "bold" }}>Release Date: </span>
-              <span>{movieReleaseDate}</span>
-            </p>
-            <p>{movieOverView}</p>
-            <span>
-              <span style={{ fontWeight: "bold" }}>{movieRating}</span>
-              &nbsp;/&nbsp;10&nbsp;({movieVoteCount} total votes)
-            </span>
-          </div>
-        </div>
-      </article>
-    </div>
-  );
+	return (
+		<div
+			className="modal-background"
+			onClick={() => {
+				props.setShouldDetailedCardShow(false);
+			}}
+		>
+			<article
+				className="modal-container"
+				onClick={(e) => {
+					e.stopPropagation();
+				}}
+			>
+				<div className="modal-name-and-close-container">
+					<span className="modal-movie-name">{movieName}</span>
+					<img
+						className="modal-close-button"
+						src="./multiply.svg"
+						alt=""
+						onClick={() => {
+							props.setShouldDetailedCardShow(false);
+						}}
+					/>
+				</div>
+				<div className="modal-movie-description">
+					<span className="modal-movie-poster-container">
+						<img
+							className="modal-movie-poster"
+							src={moviePosterURL}
+							alt="Movie Poster"
+						/>
+					</span>
+					<div className="modal-movie-description-text">
+						<p style={{ marginTop: "10px", marginBottom: "10px" }}>
+							<span style={{ fontWeight: "bold" }}>Release Date: </span>
+							<span>{movieReleaseDate}</span>
+						</p>
+						<p>{movieOverView}</p>
+						<span>
+							<span style={{ fontWeight: "bold" }}>{movieRating}</span>
+							&nbsp;/&nbsp;10&nbsp;({movieVoteCount} total votes)
+						</span>
+					</div>
+				</div>
+			</article>
+		</div>
+	);
 };
 export default MovieDetailedCard;
--- src/components/TrendingMovies.jsx
+++ src/components/TrendingMovies.jsx
@@ -3,104 +3,100 @@ import MovieCard from "./MovieCard";
 import MovieDetailedCard from "./MovieDetailedCard";
 
 function TrendingMovies(props) {
-  const [searchResults, setSearchResults] = useState();
-  const [detailedMovieId, setDetailedMovieId] = useState(76600);
-  const [shouldDetailedCardShow, setShouldDetailedCardShow] = useState(false);
-  const [titleText, setTitleText] = useState(`Most Recent Movies`);
+	const [searchResults, setSearchResults] = useState([]);
+	const [detailedMovieId, setDetailedMovieId] = useState(76600);
+	const [shouldDetailedCardShow, setShouldDetailedCardShow] = useState(false);
+	const [titleText, setTitleText] = useState(`Most Recent Movies`);
 
-  function getTrendingMovieInfo() {
-    fetch(`/.netlify/functions/gettrendingmoviesinfo`)
-      .then((result) => {
-        return result.json();
-      })
-      .then((result) => {
-        return result.trendingMovies;
-      })
-      .then((data) => {
-        setSearchResults(
-          data.results.map((_, index) => {
-            if (data.results[index].poster_path) {
-              return (
-                <MovieCard
-                  setShouldDetailedCardShow={setShouldDetailedCardShow}
-                  setDetailedMovieId={setDetailedMovieId}
-                  key={data.results[index].id}
-                  posterPath={data.results[index].poster_path}
-                  movieTitle={data.results[index].title}
-                  voteAverage={data.results[index].vote_average}
-                  movieID={data.results[index].id}
-                />
-              );
-            } else {
-              return null;
-            }
-          })
-        );
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+	async function getTrendingMovieInfo() {
+		try {
+			const result = await fetch(`/.netlify/functions/gettrendingmoviesinfo`)
+			const convertedJsonData = await result.json();
+			const data = convertedJsonData.trendingMovies;
+			setSearchResults(
+				data.results.map((_, index) => {
+					if (data.results[index].poster_path) {
+						return (
+							<MovieCard
+								setShouldDetailedCardShow={setShouldDetailedCardShow}
+								setDetailedMovieId={setDetailedMovieId}
+								key={data.results[index].id}
+								posterPath={data.results[index].poster_path}
+								movieTitle={data.results[index].title}
+								voteAverage={data.results[index].vote_average}
+								movieID={data.results[index].id}
+							/>
+						);
+					} else {
+						return null;
+					}
+				}
+				)
+			)
+		} catch (err) {
+			console.log(err);
+		};
+	}
 
-  function searchMovie(enteredMovieName) {
-    fetch(`/.netlify/functions/searchmovie?moviename=${enteredMovieName}`)
-      .then((result) => {
-        return result.json();
-      })
-      .then((result) => {
-        return result.foundMovies;
-      })
-      .then((data) => {
-        console.log(data);
-        setSearchResults(
-          data.results.map((_, index) => {
-            if (data.results[index].poster_path) {
-              return (
-                <MovieCard
-                  setShouldDetailedCardShow={setShouldDetailedCardShow}
-                  setDetailedMovieId={setDetailedMovieId}
-                  key={data.results[index].id}
-                  posterPath={data.results[index].poster_path}
-                  movieTitle={data.results[index].original_title}
-                  voteAverage={data.results[index].vote_average}
-                  movieID={data.results[index].id}
-                />
-              );
-            } else {
-              return null;
-            }
-          })
-        );
-      })
-      .catch((err) => {
-        console.log("Error Occured");
-        console.log(err);
-      });
-  }
+	function searchMovie(enteredMovieName) {
+		fetch(`/.netlify/functions/searchmovie?moviename=${enteredMovieName}`)
+			.then((result) => {
+				return result.json();
+			})
+			.then((result) => {
+				return result.foundMovies;
+			})
+			.then((data) => {
+				console.log(data);
+				setSearchResults(
+					data.results.map((_, index) => {
+						if (data.results[index].poster_path) {
+							return (
+								<MovieCard
+									setShouldDetailedCardShow={setShouldDetailedCardShow}
+									setDetailedMovieId={setDetailedMovieId}
+									key={data.results[index].id}
+									posterPath={data.results[index].poster_path}
+									movieTitle={data.results[index].original_title}
+									voteAverage={data.results[index].vote_average}
+									movieID={data.results[index].id}
+								/>
+							);
+						} else {
+							return null;
+						}
+					})
+				);
+			})
+			.catch((err) => {
+				console.log("Error Occured");
+				console.log(err);
+			});
+	}
 
-  useEffect(() => {
-    if (props.searchMovieName !== "") {
-      setTitleText(`Results for ${props.searchMovieName.replace("+", " ")}...`);
-      searchMovie(props.searchMovieName);
-    }
-  }, [props.searchMovieName]);
+	useEffect(() => {
+		if (props.searchMovieName !== "") {
+			setTitleText(`Results for ${props.searchMovieName.replace("+", " ")}...`);
+			searchMovie(props.searchMovieName);
+		}
+	}, [props.searchMovieName]);
 
-  useEffect(() => {
-    getTrendingMovieInfo();
-  }, []);
+	useEffect(() => {
+		getTrendingMovieInfo();
+	}, []);
 
-  return (
-    <main className="effective-page-body">
-      <h2 className="most-recent-movies-title">{titleText}</h2>
-      <div className="search-results-container">{searchResults}</div>
-      {shouldDetailedCardShow && (
-        <MovieDetailedCard
-          shouldDetailedCardShow={shouldDetailedCardShow}
-          setShouldDetailedCardShow={setShouldDetailedCardShow}
-          movieID={detailedMovieId}
-        />
-      )}
-    </main>
-  );
+	return (
+		<main className="effective-page-body">
+			<h2 className="most-recent-movies-title">{titleText}</h2>
+			<div className="search-results-container">{searchResults}</div>
+			{shouldDetailedCardShow && (
+				<MovieDetailedCard
+					shouldDetailedCardShow={shouldDetailedCardShow}
+					setShouldDetailedCardShow={setShouldDetailedCardShow}
+					movieID={detailedMovieId}
+				/>
+			)}
+		</main>
+	);
 }
 export default TrendingMovies;
